46f217b6c938a71df65ccf1b542b7079a4b3bfe6
DefectingCat <i@defect.ink>
2023-01-29 17:08:27 +0800

    Update pretty

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,md,mdx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx,md,mdx}'],
   theme: {
     extend: {
       colors: {
         bluish: {
-          gray: "rgba(245,247,250)",
+          gray: 'rgba(245,247,250)',
         },
         rua: {
           gray: {
-            100: "#aabfc5",
-            600: "rgb(66,66,66)",
-            700: "hsl(220, 13%, 18%)", // code background in dark
-            800: "rgb(35,38,38)", // card background in dark
-            900: "rgb(24,25,26)", // body background in dark
+            100: '#aabfc5',
+            600: 'rgb(66,66,66)',
+            700: 'hsl(220, 13%, 18%)', // code background in dark
+            800: 'rgb(35,38,38)', // card background in dark
+            900: 'rgb(24,25,26)', // body background in dark
           },
         },
       },
