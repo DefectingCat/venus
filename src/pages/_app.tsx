ceb3872272ef8542d50fc7427c41322d2716a60b
DefectingCat <i@defect.ink>
2023-01-29 17:15:58 +0800

    Add dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app';
 import 'styles/global.css';
+import { ThemeProvider } from 'next-themes';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,28 @@
+import clsx from 'clsx';
+import { useTheme } from 'next-themes';
+import { useMemo } from 'react';
+
 function App() {
-  return <div className="text-5xl">Hello world!</div>;
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  return (
+    <>
+      <div className="text-5xl">Hello world!</div>
+      <div>Current theme: {theme}</div>
+      <button className={clsx('rounded p-4')}>
+        {currentTheme === 'dark' ? (
+          <>
+            <span onClick={() => setTheme('light')}>Light</span>
+          </>
+        ) : (
+          <>
+            <span onClick={() => setTheme('dark')}>Dark</span>
+          </>
+        )}
+      </button>
+    </>
+  );
 }
 
 export default App;
