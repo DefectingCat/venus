7a8f224c3567f1d87dc783cfa6436429e178cb11
DefectingCat <i@defect.ink>
2023-01-29 17:04:27 +0800

    Add tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import 'styles/global.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
-import Image from "next/image";
-import reactLogo from "assets/react.svg";
-import tauriLogo from "assets/tauri.svg";
-import nextLogo from "assets/next.svg";
+import { useState } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
+import Image from 'next/image';
+import reactLogo from 'assets/react.svg';
+import tauriLogo from 'assets/tauri.svg';
+import nextLogo from 'assets/next.svg';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }));
   }
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      <h1 className="text-4xl text-red-800">Welcome to Tauri!</h1>
 
       <div className="row">
         <span className="logos">
-          <a href="https://nextjs.org" target="_blank">
+          <a>
             <Image
               width={144}
               height={144}
@@ -31,7 +31,7 @@ function App() {
           </a>
         </span>
         <span className="logos">
-          <a href="https://tauri.app" target="_blank">
+          <a>
             <Image
               width={144}
               height={144}
@@ -42,7 +42,7 @@ function App() {
           </a>
         </span>
         <span className="logos">
-          <a href="https://reactjs.org" target="_blank">
+          <a>
             <Image
               width={144}
               height={144}
