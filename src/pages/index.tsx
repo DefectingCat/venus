b612baaeb576752b113867d25ac68ed72ef80591
DefectingCat <i@defect.ink>
2023-01-29 16:51:02 +0800

    Remove css file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import Image from "next/image";
-import reactLogo from "../assets/react.svg";
-import tauriLogo from "../assets/tauri.svg";
-import nextLogo from "../assets/next.svg";
+import reactLogo from "assets/react.svg";
+import tauriLogo from "assets/tauri.svg";
+import nextLogo from "assets/next.svg";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
