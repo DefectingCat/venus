521dc916b1f43b2bdcca5a00a7f4089ebffcddb7
DefectingCat <i@defect.ink>
2023-02-07 17:32:23 +0800

    Add logger

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,13 +1,15 @@
 use crate::consts::CORE_FOLDER;
 
-pub struct VConfig<'a> {
-    core_folder_name: &'a str,
+pub struct VConfig {
+    pub core_folder_name: &'static str,
+    pub log_level: String,
 }
 
-impl<'a> VConfig<'a> {
+impl VConfig {
     pub fn new() -> Self {
         Self {
             core_folder_name: CORE_FOLDER,
+            log_level: "info".to_string(),
         }
     }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,12 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+
 use commands::common::current_dir;
 use config::VConfig;
+use env_logger::Env;
+use log::info;
+use std::sync::{Arc, Mutex};
 use utils::manager::{download_latest, HttpClient};
 
 mod commands;
@@ -12,6 +16,15 @@ mod consts;
 mod utils;
 
 fn main() {
+    // Init config.
+    let config = Arc::new(Mutex::new(VConfig::new()));
+    let level = &config.lock().expect("can not lock config.").log_level;
+
+    let env = Env::default().filter_or("RUA_LOG_LEVEL", level);
+    env_logger::init_from_env(env);
+    info!("starting up.");
+    info!("V2rayR - {}", env!("CARGO_PKG_VERSION"));
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![current_dir])
         .setup(|app| {
