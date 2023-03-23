405528e71a0b5d733d9eccbfeb250bc7ba66affc
DefectingCat <i@defect.ink>
2023-03-23 15:19:10 +0800

    Format config

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,14 +1,12 @@
 use crate::consts::CORE_FOLDER;
 
 pub struct VConfig {
-    pub core_folder_name: &'static str,
     pub log_level: String,
 }
 
 impl VConfig {
     pub fn new() -> Self {
         Self {
-            core_folder_name: CORE_FOLDER,
             log_level: "info".to_string(),
         }
     }
