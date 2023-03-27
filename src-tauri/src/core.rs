f96cac87852ba40675d3868a4c815a93b663aef8
DefectingCat <i@defect.ink>
2023-03-27 12:00:10 +0800

    Fix v2ary resources location

--- src-tauri/src/core.rs
+++ src-tauri/src/core.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use anyhow::Result;
 use tauri::{api::process::{Command, CommandEvent} };
 
@@ -6,15 +8,18 @@ pub struct VCore {}
 
 impl VCore {
     pub fn build() -> Result<Self> {
+        // Set v2ray assert location with environment
+        env::set_var("V2RAY_LOCATION_ASSET", "resources");
         // `new_sidecar()` expects just the filename, NOT the whole path like in JavaScript
         let (mut rx, mut child) = Command::new_sidecar("v2ray")
-            .expect("failed to create `v2ray` binary command").args(["run" ])
+            .expect("failed to create `v2ray` binary command").args(["run", "-c", "resources/config.json" ])
             .spawn()
             .expect("Failed to spawn sidecar");
 
         tauri::async_runtime::spawn(async move {
             while let Some(event) = rx.recv().await {
                 dbg!(&event);
+                
                 if let CommandEvent::Stdout(line) = event {
                     child.write("get v2ray message".as_bytes()).unwrap();
                 }
