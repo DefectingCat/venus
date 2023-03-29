991b3193a1edb9fd47e6ce5e24feaca95b4f32ec
DefectingCat <i@defect.ink>
2023-03-29 16:22:51 +0800

    Fix rust version issue

--- src-tauri/src/core.rs
+++ src-tauri/src/core.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use anyhow::Result;
-use log::info;
+use log::{error, info};
 use tauri::api::process::{Command, CommandEvent};
 
 #[derive(Debug)]
@@ -13,18 +13,22 @@ impl VCore {
         env::set_var("V2RAY_LOCATION_ASSET", "resources");
         // `new_sidecar()` expects just the filename, NOT the whole path like in JavaScript
         let (mut rx, mut child) = Command::new_sidecar("v2ray")
-            .expect("failed to create `v2ray` binary command").args(["run", "-c", "resources/config.json" ])
+            .expect("failed to create `v2ray` binary command")
+            .args(["run", "-c", "resources/config.json"])
             .spawn()
             .expect("Failed to spawn sidecar");
 
         tauri::async_runtime::spawn(async move {
             while let Some(event) = rx.recv().await {
                 // dbg!(&event);
-                if let CommandEvent::Stdout(line) = event {
-                    info!("{line}");
-                    child.write("get v2ray message".as_bytes()).unwrap();
+                match event {
+                    CommandEvent::Stdout(line) => {
+                        // info!("{line}");
+                    }
+                    _ => {
+                        // error!("Core unknown error")
+                    }
                 }
-                
             }
         });
 
