753a87a143a06d88c4d82f982a5794997f3c2a75
DefectingCat <i@defect.ink>
2023-05-23 15:57:14 +0800

    add failed tips

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ function App() {
       message.success('Add subscripition success');
     } catch (err) {
       console.error(err);
-      message.success('Failed to add subscripition');
+      message.error(`Failed to add subscripition ${err?.toString()}`);
     } finally {
       setLoading(false);
       setStatus('');
