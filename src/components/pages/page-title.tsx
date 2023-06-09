d20e39429a793e584adaebf1d9b845dd87ba039b
DefectingCat <i@defect.ink>
2023-06-09 21:37:09 +0800

    update text color in dark mode

--- src/components/pages/page-title.tsx
+++ src/components/pages/page-title.tsx
@@ -2,15 +2,15 @@ import clsx from 'clsx';
 import { ReactNode } from 'react';
 
 const Title = ({ children }: { children: ReactNode }) => {
-  return <h1 className={clsx('text-4xl')}>{children}</h1>;
+  return <h1 className={clsx('text-4xl text-gray-300')}>{children}</h1>;
 };
 
 Title.h1 = ({ children }: { children: ReactNode }) => {
-  return <h1 className={clsx('text-4xl')}>{children}</h1>;
+  return <h1 className={clsx('text-4xl text-gray-400')}>{children}</h1>;
 };
 
 Title.h2 = ({ children }: { children: ReactNode }) => {
-  return <h2 className={clsx('text-xl text-gray-700')}>{children}</h2>;
+  return <h2 className={clsx('text-xl text-gray-400')}>{children}</h2>;
 };
 
 export default Title;
