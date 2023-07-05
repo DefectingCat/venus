e27d654fbe6d7d2034c46237d1afe073d7c42aee
DefectingCat <i@defect.ink>
2023-07-05 14:32:34 +0800

    feat: add realtime logs

--- src/pages/logging.tsx
+++ src/pages/logging.tsx
@@ -13,10 +13,28 @@ const Logging = () => {
           <Title>Logging</Title>
         </div>
 
-        <div className={clsx('flex-1 rounded-lg bg-gray-200', 'p-6')}>
-          {logs.map((log) => (
-            <div>{log}</div>
-          ))}
+        <div
+          className={clsx(
+            'flex-1 rounded-lg bg-gray-200',
+            'p-6 overflow-x-auto'
+          )}
+        >
+          {/* <pre className="m-0"> */}
+          {/*   <List data={logs} height={300} itemHeight={30} itemKey="id"> */}
+          {/*     {(log) => ( */}
+          {/*       <div className={clsx('h-[30px]')}> */}
+          {/*         <code>{log}</code> */}
+          {/*       </div> */}
+          {/*     )} */}
+          {/*   </List> */}
+          {/* </pre> */}
+          <pre className="m-0">
+            {logs.map((log) => (
+              <div className={clsx('px-2', 'pb-2 last:pb-0')}>
+                <code>{log}</code>
+              </div>
+            ))}
+          </pre>
         </div>
       </div>
     </MainLayout>
