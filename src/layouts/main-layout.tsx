abbd84ce9f2d214f894c6608c52ea4b8422582cd
DefectingCat <i@defect.ink>
2023-07-09 12:53:26 +0800

    feat: add drag bar on macos

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -2,20 +2,31 @@ import clsx from 'clsx';
 import SideBar from 'components/side-bar';
 import { ReactNode } from 'react';
 
+/**
+ * Entier App layout
+ */
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main
-      className={clsx(
-        'flex w-[100vw] h-[100vh]',
-        'bg-bluish-gray dark:bg-rua-gray-900'
-      )}
-    >
-      {/* navi */}
-      <SideBar />
+    <>
+      <main
+        className={clsx(
+          'flex w-[100vw] h-[100vh]',
+          'bg-bluish-gray dark:bg-rua-gray-900',
+          'relative'
+        )}
+      >
+        <div
+          data-tauri-drag-region
+          className={clsx('h-7 bg-transparent fixed', 'top-0 left-0 right-0')}
+        ></div>
 
-      {/* body */}
-      <div className="p-8 flex-1 overflow-auto h-full">{children}</div>
-    </main>
+        {/* navi */}
+        <SideBar />
+
+        {/* body */}
+        <div className="p-8 flex-1 overflow-auto h-full">{children}</div>
+      </main>
+    </>
   );
 };
 
