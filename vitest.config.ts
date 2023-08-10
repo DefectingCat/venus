548a95c2154f22e63fc40255ac19450f06c8e638
DefectingCat <xfyxfy@proton.me>
2023-08-10 13:52:11 +0800

    fix vitest type error

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react() as any, tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
