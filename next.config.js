f49c06f77beb0c1d6109aed82e00722b3326349b
DefectingCat <i@defect.ink>
2023-08-10 15:03:38 +0800

    deps: update nextjs to 13.4
    fix vitest type error

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
+  output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true,
   },
+  transpilePackages: ['ahooks']
 };
 
-module.exports = nextConfig;
+
+module.exports = nextConfig
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react() as any, tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
