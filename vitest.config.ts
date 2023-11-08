33d2ba5f72aeb7459320d14fe2ed52468174d06e
DefectingCat <i@defect.ink>
2023-11-08 13:46:08 +0800

    fix vitest type error
    update dependenceis

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,9 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-//@ts-ignore
 export default defineConfig({
-  //@ts-ignore
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
