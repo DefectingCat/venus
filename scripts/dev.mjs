5b38aa7ef9110fb44ea863466a95569681ada6dd
xfy <i@rua.plus>
2024-03-21 10:56:18 +0800

    build(monaco): add map files in dev env

--- scripts/dev.mjs
+++ scripts/dev.mjs
@@ -1,41 +1,72 @@
 import fs from 'fs/promises';
+import fsSync from 'fs';
 import path from 'path';
 
 const rootPath = process.cwd();
-const mona = path.resolve(rootPath, './node_modules/monaco-editor/min/vs');
-const publicFolder = path.resolve(rootPath, './public/vs');
 
-export async function devMain() {
-  await fs.cp(`${mona}/language/json`, `${publicFolder}/language/json`, {
-    recursive: true,
-  });
+/**
+ * @args -p production mode
+ */
+const args = process.argv.slice(2);
+const production = args.includes('-p');
+
+export async function devMain(mona, publicPath, andMap = false) {
   await fs.cp(
-    `${mona}/editor/editor.main.js`,
-    `${publicFolder}/editor/editor.main.js`,
+    `${mona}/editor/editor.main.js${andMap ? '.map' : ''}`,
+    `${publicPath}/editor/editor.main.js${andMap ? '.map' : ''}`,
     {
       recursive: true,
     },
   );
   await fs.cp(
-    `${mona}/editor/editor.main.nls.js`,
-    `${publicFolder}/editor/editor.main.nls.js`,
+    `${mona}/editor/editor.main.nls.js${andMap ? '.map' : ''}`,
+    `${publicPath}/editor/editor.main.nls.js${andMap ? '.map' : ''}`,
     {
       recursive: true,
     },
   );
+  await fs.cp(`${mona}/base/`, `${publicPath}/base/`, {
+    recursive: true,
+  });
   await fs.cp(
-    `${mona}/editor/editor.main.css`,
-    `${publicFolder}/editor/editor.main.css`,
+    `${mona}/loader.js${andMap ? '.map' : ''}`,
+    `${publicPath}/loader.js${andMap ? '.map' : ''}`,
     {
       recursive: true,
     },
   );
-  await fs.cp(`${mona}/base/`, `${publicFolder}/base/`, {
-    recursive: true,
-  });
-  await fs.cp(`${mona}/loader.js`, `${publicFolder}/loader.js`, {
-    recursive: true,
-  });
+  if (!andMap) {
+    await fs.cp(`${mona}/language/json`, `${publicPath}/language/json`, {
+      recursive: true,
+    });
+    await fs.cp(
+      `${mona}/editor/editor.main.css`,
+      `${publicPath}/editor/editor.main.css`,
+      {
+        recursive: true,
+      },
+    );
+  }
 }
 
-devMain();
+if (production) {
+  const publicMap = path.resolve(rootPath, './public/min-maps/');
+  if (fsSync.existsSync(publicMap)) {
+    await fs.rm(publicMap, {
+      recursive: true,
+    });
+  }
+  const mona = path.resolve(rootPath, './node_modules/monaco-editor/min/vs');
+  const publicFolder = path.resolve(rootPath, './public/vs');
+  await devMain(mona, publicFolder, false);
+} else {
+  const monaMap = path.resolve(
+    rootPath,
+    './node_modules/monaco-editor/min-maps/vs',
+  );
+  const publicMap = path.resolve(rootPath, './public/min-maps/vs');
+  await devMain(monaMap, publicMap, true);
+  const mona = path.resolve(rootPath, './node_modules/monaco-editor/min/vs');
+  const publicFolder = path.resolve(rootPath, './public/vs');
+  await devMain(mona, publicFolder, false);
+}
