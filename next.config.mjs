dc378825982d39e52d84ca5a56f020f0560c3b26
xfy <i@rua.plus>
2024-04-22 13:15:44 +0800

    build(deps): add bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import BundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = BundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -24,4 +30,4 @@ const nextConfig = {
   ],
 };
 
-module.exports = nextConfig;
+export default withBundleAnalyzer(nextConfig);
