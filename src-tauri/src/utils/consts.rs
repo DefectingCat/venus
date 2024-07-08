ad92f6b360a86c303839c133651ac3d3d1047955
xfy <i@rua.plus>
2024-07-08 09:03:31 +0800

    refactor(core): move to core only

--- src-tauri/src/utils/consts.rs
+++
@@ -1,6 +0,0 @@
-/// Default speed test url
-pub static SPEED_URL: &str = "https://sabnzbd.org/tests/internetspeed/50MB.bin";
-
-/// info from package
-pub static VERSION: &str = env!("CARGO_PKG_VERSION");
-pub static NAME: &str = env!("CARGO_PKG_NAME");
